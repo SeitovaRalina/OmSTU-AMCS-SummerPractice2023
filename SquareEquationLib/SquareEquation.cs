f80bdae12741c052d64f4be1460753568f88f731
RALINA <seitovaralina1@gmail.com>
2023-06-24 13:58:43 +0000

    please work

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -18,13 +18,13 @@ public class SquareEquation
         b = b/a; c = c/a;
         double d = b * b - 4 * c;
         
-        if (d < -eps) { solution = new double[0]; }//D < 0
-        else if (d < eps && d > -eps)//D = 0
+        if (d < -eps) { solution = new double[0]; } //D < 0
+        else if (d < eps && d > -eps) //D = 0
         {
             solution = new double[1];
             solution[0] = -b / 2;
         }
-        else //D > 0
+        else//D > 0
         {
             solution = new double[2];
             solution[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
